73cb119a0d2b703efd2904775fa9694aac83f2be
Sekwah <contact@sekwah.com>
2026-07-09 19:42:51 +0100

    chore: add extra macro logging for debug

--- structly_macros/src/lib.rs
+++ structly_macros/src/lib.rs
@@ -1,13 +1,20 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Structly, attributes(structly, structly_if))]
 pub fn derive_structly(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let struct_name = &input.ident;
 
-    eprintln!("expanding Structly for: {}", input.ident);
+    let fields = match &input.data {
+        Data::Struct(s) => match &s.fields {
+            Fields::Named(named) => &named.named,
+            _ => panic!("only named fields are supported"),
+        },
+        _ => panic!("only structs are supported"),
+    };
+    eprintln!("{:#?}", input);
 
     quote! {
         impl ::structly::Verify for #struct_name {
